323c3371ebe45cc6c888a9cac3e91accb23c034a
MuhammadBurhan235 <muhammadburhan@student.telkomuniversity.ac.id>
2023-03-27 01:56:19 +0700

    Menambahkan Class SayaTubeVideo

--- tpmodul6_1302213109/Program.cs
+++ tpmodul6_1302213109/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("FIRST COMMIT!");
+﻿using tpmodul6_1302213109;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        string videoTitle = "Tutorial Design By Contract - Muhammad Burhan";
+        SayaTubeVideo myVideo = new SayaTubeVideo(videoTitle);
+        myVideo.IncreasePlayCount(1000);
+        for (int i = 0; i < 20000000; i += 10000000)
+        {
+            try
+            {
+                myVideo.IncreasePlayCount(10000000);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                break;
+            }
+        }
+        myVideo.PrintVideoDetails();
+
+        Console.ReadLine();
+    }
+}
